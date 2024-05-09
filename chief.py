c4ae6aca597dcbc6adb022b498dc37eab6a2801b
Valerii Dzhalaganiia <dzhvalera199292@gmail.com>
2024-05-09 22:18:27 +0200

    Final version

--- chief.py
+++ chief.py
@@ -1,7 +1,21 @@
-def chiefHopper(arr):
-    # Write your code here
-    su = arr[0]
-    for i in range(len(arr) - 1):
-        su += arr[i + 1] - arr[i]
+import math
+from decimal import Decimal
 
-    return su
+arr = list([i for i in range(100000, 0, -1)])
+
+
+curr = 0
+for i in arr:
+    if i >= curr:
+        curr = curr - (i - curr)
+
+if curr < 0:
+    curr = curr * (-1)
+    step = 2 ** len(arr)
+    step = Decimal(step)
+    curr = Decimal(curr)
+
+    an = curr/step
+    print(an)
+
+
